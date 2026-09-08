dd75b642596af09c8797baeb7ece925d50345218
anhdong <mobundong@gmail.com>
2026-09-08 23:14:32 +0700

    add xdg-desktop-portal-wlr

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -14,6 +14,7 @@
 
     xdg-desktop-portal
     xdg-desktop-portal-gtk
+    xdg-desktop-portal-wlr
     gnome-keyring
     polkit_gnome
 
---
+++ modules/nixos/keyboard.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+{
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+}
