de0c9d2b9e4ff53cd51f20bcdf9e0e85b98612f6
anhdong <mobundong@gmail.com>
2026-09-12 17:35:21 +0700

    Add fcitx5 and unikey

--- modules/nixos/keyboard.nix
+++ modules/nixos/keyboard.nix
@@ -1,6 +1,20 @@
 { config, pkgs, ... }:
 {
+  # Set up FCITX 5
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
 
+    fcitx5 = {
+      waylandFrontend = true;
+
+      addons = with pkgs; [
+	qt6Packages.fcitx5-unikey
+        fcitx5-gtk
+      ];
+    };
+  };
+  
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
